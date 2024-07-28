5f881a0a1b61c9741181d93ae81a9d7bc76b122d
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2024-07-28 14:56:17 +0600

    Updated reader wrapper on the client side

--- client.js
+++ client.js
@@ -1,14 +1,14 @@
 async function* streamingFetch( fetchcall ) {
 
     const response = await fetchcall();
-    // Attach Reader
+
     const reader = response.body.getReader();
     while (true) {
-        // wait for next encoded chunk
+ 
         const { done, value } = await reader.read();
-         // check if stream is done
+  
         if (done) break;
-        // Decodes data chunk and yields it
+
         yield (new TextDecoder().decode(value));
     }
 }
