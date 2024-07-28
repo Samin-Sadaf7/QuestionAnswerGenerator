8521774f451739ab380be91856a89a0a23a65c00
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2024-07-28 14:58:10 +0600

    Delete routes file

--- routes/stream.js
+++
@@ -1,8 +0,0 @@
-async function* generateData() {
-    for (let i = 0; i < 1000; i++) {
-        // Simulate delay
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        // Yield data chunk
-        yield `data chunk ${i}\n`;
-    }
-}
