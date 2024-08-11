520cb0114e5cf84adcce601151bbf09347958f2e
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2024-08-11 14:45:03 +0600

    Introduced worker thread to make non blocking route non blocking

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config()
+const {Worker} = require('worker_threads');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/nonblocking', (req, res) =>{
+    res.status(200).send("Non blocking Page");
+});
+
+app.get('/blocking', async(req, res) =>{
+
+    const worker = new Worker('./worker.js');
+    worker.on("message", (data) =>{
+        res.status(200).send(`the count is now : ${data}`);
+    });
+    worker.on("error", (err) =>{
+        res.status(404).send(err.message);
+    });
+});
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+});
